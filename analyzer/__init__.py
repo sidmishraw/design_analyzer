77bf775486bf59e99469864c2e40d7121fa8b0d3
Sidharth Mishra <sidmishraw@gmail.com>
2017-03-29 21:47:54 -0700

    started some implementations.

--- analyzer/__init__.py
+++ analyzer/__init__.py
@@ -3,4 +3,10 @@
 # @Author: Sidharth Mishra
 # @Date:   2017-03-25 18:02:00
 # @Last Modified by:   Sidharth Mishra
-# @Last Modified time: 2017-03-25 18:13:10
+# @Last Modified time: 2017-03-29 21:04:55
+
+
+
+
+__author__ = 'sidmishraw'
+__version__ = '1.0.0'
